cfc0cede65c0cf7bb429581ad9e9066dad5fd1c8
Deebyanshu <jhadeebyanshu@gmail.com>
2026-02-27 21:08:44 +0530

    basic structure done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Start of backend project");
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+let myusername = process.env.MY_USERNAME;
+console.log(myusername);
+console.log("Start of fantastic backend project");
