f2707ce99007fa7913e1c56caa849f20cf78ad44
Deebyanshu <jhadeebyanshu@gmail.com>
2026-03-01 20:25:33 +0530

    package updates

--- index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config({
-  path: "./.env",
-});
-
-let myusername = process.env.MY_USERNAME;
-console.log(myusername);
-console.log("Start of fantastic backend project");
